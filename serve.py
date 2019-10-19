f89a827477da7170cb28e09a992d05769566fa87
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2019-10-20 02:07:46 +0530

    Barebones backend

---
+++ serve.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    message = "The Flask Shop"
+    return render_template('index.html', message=message)
+
+
+# run the application
+if __name__ == "__main__":
+    app.run(debug=True)
